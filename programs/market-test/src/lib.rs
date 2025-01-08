b99b5ee1850719e21ab8491b18c8a47a152085b2
joeAtl <joe.chen@atlancube.com>
2025-01-09 03:50:41 +0800

    need to be reviewed

--- programs/market-test/src/lib.rs
+++ programs/market-test/src/lib.rs
@@ -1,28 +1,109 @@
-use anchor_lang::prelude::*;
-use anchor_spl::token;
+use anchor_lang::{accounts::signer, prelude::*};
+use anchor_spl::{
+    dex::{initialize_market, Dex, InitializeMarket, InitializeMarketBumps},
+    token::{Mint, Token, TokenAccount},
+};
 
-declare_id!("8HePVemepFh6CRn9t1kQRgyWrMjr4rjatQCC2t7N7gEw");
+use std::mem::size_of;
+
+declare_id!("Az88Wvk5gVMqFc7QQ94NqSa3sVCjnoQhjfNdqiti26fu");
 
 #[program]
 pub mod market_test {
     use super::*;
 
-    pub fn initialize(ctx: Context<CreateMarketId>) -> Result<()> {
-        // msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        coin_lot_size: u64,
+        pc_lot_size: u64,
+        pc_dust_threshold: u64,
+        vault_signer_nonce: u64,
+    ) -> Result<()> {
+        ctx.accounts.initialize(
+            coin_lot_size,
+            pc_lot_size,
+            pc_dust_threshold,
+            vault_signer_nonce,
+        )
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateMarketId <'info> {
-    pub user: Signer<'info>,
+#[instruction(nonce: u64)]
+pub struct Initialize<'info> {
+    // Removed lifetime parameter
+    #[account(mut)]
+    ///CHECKED
+    pub market: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub event_queue: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub req_queue: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub bids: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub asks: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub coin_vault: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    ///CHECKED
+    pub pc_vault: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub coin_mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub pc_mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub openbook_dex: Program<'info, Dex>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
-impl  <'info> CreateMarketId <'info> {
-    pub fn create_id(&mut self) -> Result<()> {
-        Ok(())
+impl<'info> Initialize<'info> {
+    pub fn initialize(
+        &mut self,
+        coin_lot_size: u64,
+        pc_lot_size: u64,
+        pc_dust_threshold: u64,
+        vault_signer_nonce: u64,
+    ) -> Result<()> {
+        let cpi_accounts = InitializeMarket {
+            market: self.market.to_account_info(),
+            coin_mint: self.coin_mint.to_account_info(),
+            coin_vault: self.coin_vault.to_account_info(),
+            pc_mint: self.pc_mint.to_account_info(),
+            pc_vault: self.pc_vault.to_account_info(),
+            bids: self.bids.to_account_info(),
+            asks: self.asks.to_account_info(),
+            req_q: self.req_queue.to_account_info(),
+            event_q: self.event_queue.to_account_info(),
+            rent: self.rent.to_account_info(),
+        };
+        // let seeds: [&[&[u8]]; 1] = [&[self.market.key().as_ref(), &[bumps.pda_signer]]];
+        // // let signer_seeds = &seeds;
+
+        let cpi_ctx = CpiContext::new(self.openbook_dex.to_account_info(), cpi_accounts);
+        initialize_market(
+            cpi_ctx,
+            coin_lot_size,
+            pc_lot_size,
+            vault_signer_nonce,
+            pc_dust_threshold,
+        )
     }
-    
 }
-
-pub struct Initialize {}
--- tests/market-test.ts
+++ tests/market-test.ts
@@ -1,16 +1,332 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import {
+  ComputeBudgetProgram,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmTransaction,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
 import { MarketTest } from "../target/types/market_test";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import {
+  createAssociatedTokenAccountIdempotentInstruction,
+  createInitializeMint2Instruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+  createAccount,
+} from "@solana/spl-token";
+import { MARKET_STATE_LAYOUT_V3 } from "@openbook-dex/openbook";
+import { randomBytes } from "crypto";
 
 describe("market-test", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.MarketTest as Program<MarketTest>;
+  const connection = provider.connection;
+
+  const DEX_PROGRAM_ID = new PublicKey(
+    "EoTcMgcDRTJVZDMZWBoU6rhYHZfkNTVEAfz3uUJRcYGj"
+  );
+  // const DEX_PROGRAM_ID = new PublicKey(
+  //   "EoTcMgcDRTJVZDMZWBoU6rhYHZfkNTVEAfz3uUJRcYGj"
+  // );
+  const user = provider.wallet as NodeWallet;
+
+  const seed = new anchor.BN(randomBytes(4));
+  console.log("SEED", seed.toNumber());
+  const MARKET_SEED = `market_${seed.toNumber()}`;
+  const BIDS_SEED = `bids_${seed.toNumber()}`;
+  const ASKS_SEED = `asks_${seed.toNumber()}`;
+  const REQ_Q_SEED = `req_q_${seed.toNumber()}`;
+  const EVENT_Q_SEED = `event_q_${seed.toNumber()}`;
+
+  const COIN_LOT_SIZE = new anchor.BN(10);
+  const PC_LOT_SIZE = new anchor.BN(100_000);
+  const PC_DUST_THRESHOLD = new anchor.BN(10);
+
+  // const market = Keypair.generate();
+
+  const coin_mint = Keypair.generate();
+
+  const pc_mint = Keypair.generate();
+  let accounts;
+
+  const createProgramAccount = (
+    basedPublicKey,
+    newAccountPubkey,
+    lamports,
+    space,
+    seed,
+    programId
+  ) => {
+    return SystemProgram.createAccountWithSeed({
+      fromPubkey: user.publicKey,
+      basePubkey: basedPublicKey,
+      newAccountPubkey,
+      seed,
+      lamports,
+      space,
+      programId,
+    });
+  };
+
+  const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${connection.rpcEndpoint}`
+    );
+    return signature;
+  };
+
+  const [userAtaCoin, userAtaPc] = [user.publicKey]
+    .map((a) =>
+      [coin_mint, pc_mint].map((m) =>
+        getAssociatedTokenAddressSync(m.publicKey, a, false, TOKEN_PROGRAM_ID)
+      )
+    )
+    .flat();
+  let market;
+  let bids;
+  let asks;
+  let req_q;
+  let event_q;
+  let coin_vault;
+  let pc_vault;
+  let signer_vault_account;
+  let nonce;
+  coin_vault = Keypair.generate();
+  pc_vault = Keypair.generate();
+
+  beforeEach(async () => {
+    market = await PublicKey.createWithSeed(
+      user.publicKey,
+      MARKET_SEED,
+      DEX_PROGRAM_ID
+    );
+
+    bids = await PublicKey.createWithSeed(
+      user.publicKey,
+      BIDS_SEED,
+      DEX_PROGRAM_ID
+    );
+    asks = await PublicKey.createWithSeed(
+      user.publicKey,
+      ASKS_SEED,
+      DEX_PROGRAM_ID
+    );
+    req_q = await PublicKey.createWithSeed(
+      user.publicKey,
+      REQ_Q_SEED,
+      DEX_PROGRAM_ID
+    );
+    event_q = await PublicKey.createWithSeed(
+      user.publicKey,
+      EVENT_Q_SEED,
+      DEX_PROGRAM_ID
+    );
+
+    // important, should 
+    [signer_vault_account, nonce] = PublicKey.findProgramAddressSync(
+      [market.toBuffer()],
+      DEX_PROGRAM_ID
+    )
+
+    //mint progress
+    let lamports = await getMinimumBalanceForRentExemptMint(connection);
+    let tx = new Transaction();
+    tx.instructions = [
+      ...[user].map((account) =>
+        SystemProgram.transfer({
+          fromPubkey: provider.publicKey,
+          toPubkey: account.publicKey,
+          lamports: 10 * LAMPORTS_PER_SOL,
+        })
+      ),
+      ...[coin_mint, pc_mint].map((mint) =>
+        SystemProgram.createAccount({
+          fromPubkey: provider.publicKey,
+          newAccountPubkey: mint.publicKey,
+          lamports,
+          space: MINT_SIZE,
+          programId: TOKEN_PROGRAM_ID,
+        })
+      ),
+      ...[
+        { mint: coin_mint, authority: user.publicKey, ata: userAtaCoin },
+        { mint: pc_mint, authority: user.publicKey, ata: userAtaPc },
+      ].flatMap((x) => [
+        createInitializeMint2Instruction(
+          x.mint.publicKey,
+          6,
+          x.authority,
+          null,
+          TOKEN_PROGRAM_ID
+        ),
+        createAssociatedTokenAccountIdempotentInstruction(
+          provider.publicKey,
+          x.ata,
+          x.authority,
+          x.mint.publicKey,
+          TOKEN_PROGRAM_ID
+        ),
+        createMintToInstruction(
+          x.mint.publicKey,
+          x.ata,
+          x.authority,
+          1e9,
+          undefined,
+          TOKEN_PROGRAM_ID
+        ),
+      ]),
+    ];
+    
+    await provider
+      .sendAndConfirm(tx, [user.payer, coin_mint, pc_mint])
+      .then(log);
+    //tx 1
+    await createAccount(
+      connection,
+      user.payer,
+      coin_mint.publicKey,
+      signer_vault_account,
+      coin_vault
+    );
+    await createAccount(
+      connection,
+      user.payer,
+      pc_mint.publicKey,
+      signer_vault_account,
+      pc_vault
+    );
+
+    accounts = {
+      market: market,
+      coinMint: coin_mint.publicKey,
+      pcMint: pc_mint.publicKey,
+      coinVault: coin_vault.publicKey,
+      pcVault: pc_vault.publicKey,
+      bids: bids,
+      asks: asks,
+      reqQueue: req_q,
+      eventQueue: event_q,
+      authority: user.payer.publicKey,
+    };
+
+    //tx2
+    const tx2 = new Transaction();
+
+    console.log("txxxxx");
+    tx2.add(
+      createProgramAccount(
+        user.publicKey,
+        market,
+        await connection.getMinimumBalanceForRentExemption(
+          388
+          // MARKET_STATE_LAYOUT_V3.span
+        ),
+        // MARKET_STATE_LAYOUT_V3.span,
+        388,
+        MARKET_SEED,
+        DEX_PROGRAM_ID
+      ),
+      createProgramAccount(
+        user.publicKey,
+        req_q,
+        await connection.getMinimumBalanceForRentExemption(764),
+        // await connection.getMinimumBalanceForRentExemption(5120 + 12),
+        764,
+        REQ_Q_SEED,
+        DEX_PROGRAM_ID
+      ),
+      createProgramAccount(
+        user.publicKey,
+        event_q,
+        await connection.getMinimumBalanceForRentExemption(11308),
+        // await connection.getMinimumBalanceForRentExemption(262144 + 12),
+        11308,
+        EVENT_Q_SEED,
+        DEX_PROGRAM_ID
+      ),
+      createProgramAccount(
+        user.publicKey,
+        bids,
+        await connection.getMinimumBalanceForRentExemption(14524),
+        // await connection.getMinimumBalanceForRentExemption(65536 + 12),
+        14524,
+        BIDS_SEED,
+        DEX_PROGRAM_ID
+      ),
+      createProgramAccount(
+        user.publicKey,
+        asks,
+        await connection.getMinimumBalanceForRentExemption(14524),
+        // await connection.getMinimumBalanceForRentExemption(65536 + 12),
+        14524,
+        ASKS_SEED,
+        DEX_PROGRAM_ID
+      )
+    );
+    console.log("tx222222222");
+    await sendAndConfirmTransaction(connection, tx2, [user.payer]);
+    console.log("tx000000000");
+  });
 
   it("Is initialized!", async () => {
+    try {
+      console.log("accounts", accounts);
+      const tx = new Transaction().add(
+        ComputeBudgetProgram.setComputeUnitLimit({ units: 200_000 }),
+        ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 200_000 }),
+        await program.methods
+          .initialize(
+            COIN_LOT_SIZE,
+            PC_LOT_SIZE,
+            PC_DUST_THRESHOLD,
+            new anchor.BN(nonce),
+          )
+          .accounts(accounts)
+          .instruction()
+      );
+      tx.feePayer = user.publicKey;
+      tx.recentBlockhash = (
+        await provider.connection.getLatestBlockhash()
+      ).blockhash;
+      console.log(await provider.connection.simulateTransaction(tx));
+      const sig = await sendAndConfirmTransaction(
+        provider.connection,
+        tx,
+        [user.payer],
+        {
+          skipPreflight: true,
+        }
+      );
+      console.log(
+        "Successfully in initialize : ",
+        `https://solscan.io/tx/${sig}?cluster=devnet`
+      );
+    } catch (error) {
+      console.log("Error in initialize", error);
+    }
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
   });
 });
+
+const ggg = {
+  //on curve
+  marketId: "6JV2zbo3CJassywQ5gWZJ7QaK1aF9wxYtNuA5yBWyiLi",
+  requestQueue: "AqPCeoPpi4pNR4fmVvYx6vghF1USjTVQNdqg8e9ctEPP",
+  eventQueue: "6CpmJyFpmQEB3kdoNWuYKHbP4Tk9xez7CtQuSHbVizUA",
+  bids: "J9p7doHGWzPkdtxCUhyPh9AStshxznZnjeZStRQezrHo",
+  asks: "6wDrkH4ZfDxuRaxyKEKbNiLgkFBaW34NhRZnqrWEuBCn",
+  baseVault: "FuhuGpxEhPaaZPDvX5ygGBqtmT7VgRHUzKpEkpXdBLgk",
+  quoteVault: "9BFcmo2aHV3JexBm88FBvEJyfS4sA3PBxqi6yt1pc9hs",
+  baseMint: "BehfkowGJ6HAwfdhY23J8XzPXvZGRXNoZpPtKR9QGst3",
+  quoteMin: "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU",
+};
